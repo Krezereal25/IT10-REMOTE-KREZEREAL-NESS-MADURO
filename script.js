eabbf1d05fccac938633f3bc2be415b54737310d
krez <krezerealm@gmail.com>
2025-05-14 10:38:48 +0800

    Added JavaScript to handle form submit and assign correct name variable

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("nameForm").addEventListener(submit,function(event){event.preventDefault.Default();}
+
+const input = document.getElementById("name");
+const nameInput = input.ariaValueMax.trim();
+const correctname ="Krez";
+
